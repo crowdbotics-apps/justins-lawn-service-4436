9a220694db78eb2834bed834bcb43219ce08d294
Crowdbotics <team@crowdbotics.com>
2019-06-09 14:25:25 +0000

    initial commit

---
+++ justins_lawn_service_4436Tests/justins_lawn_service_4436Tests.swift
@@ -0,0 +1,34 @@
+//
+//  justins_lawn_service_4436Tests.swift
+//  justins_lawn_service_4436Tests
+//
+//  Created by Dan on 10/23/18.
+//  Copyright © 2018 Crowdbotics. All rights reserved.
+//
+
+import XCTest
+@testable import justins_lawn_service_4436
+
+class justins_lawn_service_4436Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
